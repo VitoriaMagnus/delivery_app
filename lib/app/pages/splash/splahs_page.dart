47ad009eaf831267a5b11f3809d9c457b27b4858
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2023-02-01 11:14:35 -0300

    Project config, theme and UI extensions

---
+++ lib/app/pages/splash/splahs_page.dart
@@ -0,0 +1,23 @@
+import 'package:delivery_app/app/core/ui/styles/app_styles.dart';
+import 'package:delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:flutter/material.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Splash'),
+      ),
+      body: Column(
+        children: [
+          Container(),
+          ElevatedButton(onPressed: () {}, child: const Text('Teste')),
+          TextFormField(),
+        ],
+      ),
+    );
+  }
+}
