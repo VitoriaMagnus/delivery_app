f864bf902a4a3858f7bd37a3f9db3af24d326d24
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2023-02-02 09:40:47 -0300

    construção da home e seus components

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,3 +1,6 @@
+import 'package:delivery_app/app/core/widgets/delivery_app_bar.dart';
+import 'package:delivery_app/app/models/product_model.dart';
+import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -6,13 +9,28 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home Page'),
-      ),
-      body: Center(
-        child: Container(
-          child: Text('Home Page'),
-        ),
+      appBar: DeliveryAppBar(),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return DeliveryProductTile(
+                  product: ProductModel(
+                    id: 0,
+                    nome: 'Lanche X',
+                    description:
+                        'Lanche acompanha pão, hambúguer, mussarela, alface, tomate e maionese',
+                    price: 15.0,
+                    image:
+                        'https://assets.unileversolutions.com/recipes-v2/106684.jpg?imwidth=800',
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
