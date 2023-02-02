7b7ba8ed2cd82a4b9bca3b32acb23617f928cb97
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2023-02-02 10:47:05 -0300

    add helpers

--- lib/app/pages/home/home_page.dart
+++ lib/app/pages/home/home_page.dart
@@ -1,15 +1,33 @@
+import 'package:delivery_app/app/core/ui/helpers/loader.dart';
+import 'package:delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:delivery_app/app/core/widgets/delivery_app_bar.dart';
 import 'package:delivery_app/app/models/product_model.dart';
 import 'package:delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: DeliveryAppBar(),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // showLoader();
+          // await Future.delayed(Duration(seconds: 2));
+          showError('Erro ao criar qualquer coisa');
+          await Future.delayed(Duration(seconds: 2));
+          showInfo('Info ao criar qualquer coisa');
+          await Future.delayed(Duration(seconds: 2));
+          showSuccess('Sucesso ao criar qualquer coisa');
+        },
+      ),
       body: Column(
         children: [
           Expanded(
