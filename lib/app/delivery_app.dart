867ee5032d0b71ad5f33be06754fa6be15aa1d17
VitoriaMagnus <vitoria.magnus@rede.ulbra.br>
2023-02-03 21:22:35 -0300

    tela de login e register

--- lib/app/delivery_app.dart
+++ lib/app/delivery_app.dart
@@ -1,6 +1,7 @@
 import 'package:delivery_app/app/core/provider/application_binding.dart';
 import 'package:delivery_app/app/core/ui/theme/theme_config.dart';
 import 'package:delivery_app/app/pages/auth/login/login_page.dart';
+import 'package:delivery_app/app/pages/auth/login/register/register_page.dart';
 import 'package:delivery_app/app/pages/home/home_router.dart';
 import 'package:delivery_app/app/pages/product_detail/product_detail_router.dart';
 import 'package:delivery_app/app/pages/splash/splahs_page.dart';
@@ -21,6 +22,7 @@ class DeliveryApp extends StatelessWidget {
           '/home': (context) => HomeRouter.page,
           '/productDetail': (context) => ProductDetailRouter.page,
           '/auth/login': (context) => const LoginPage(),
+          '/auth/register': (context) => RegisterPage(),
         },
       ),
     );
